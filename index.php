6be2e25de645c7d512a29671bf57a2efe37c5398
Oscar <oscar.ostariz@unizar.es>
2021-01-31 10:28:34 +0000

    primer commit del proyecto 1.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+       echo "Practica 2 Despliegue de aplicaciones web";
+       echo "Añadimos un cambio al fichero";
+        ?>
+    </body>
+</html>
